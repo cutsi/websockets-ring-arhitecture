5770e713c64227c189ca8685a1392495cd32d02a
cutsi <cuturacutura.00@gmail.com>
2021-10-21 23:57:42 +0200

    adding finished nodes

---
+++ server-client4.py
@@ -0,0 +1,53 @@
+import socket
+import threading 
+import time
+
+FORMAT = "utf-8"
+IP = "127.0.0.1"
+PORT = 50300
+nextPort = 50000
+ADDR = (IP, PORT)
+ADDR_to_connect_to = (IP, nextPort)
+server_lst = []
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+server.listen()
+
+lst = []
+data_recieved = False
+data = ""
+msg = ""
+def connect():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    time.sleep(10)
+    client.connect(ADDR_to_connect_to)
+    return client
+
+def startclient():
+    client = connect()
+    
+    if (len(lst) > 0):
+        senddata(client)
+    else:
+        sendinput(client)
+
+def senddata(client):
+    client.send(bytes(lst[0], FORMAT))
+
+def sendinput(client):
+    client.send(bytes(msg, FORMAT))
+
+def startserver():
+    con, add = server.accept()
+    data = con.recv(1024).decode(FORMAT)
+    lst.append(data)
+    if(data != msg):   
+        print(data)
+    else:
+        print("DATA SUCCESSFULLY CIRCLED THE RING")
+
+
+thread = threading.Thread(target=startserver)
+thread.start()
+startclient()
